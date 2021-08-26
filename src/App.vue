f9baaa7aa35400292eb930600b3a92586f86cf27
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-08-26 10:55:38 +0100

    starter files

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<style>
+body {
+  background: #f2f2f2;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  color: #555;
+  margin-top: 0 auto;
+}
+</style>
