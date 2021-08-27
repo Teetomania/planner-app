d9af3eab60533ba9abde28bd96434ef8a2883ac4
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-08-27 12:56:59 +0100

    project done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <router-view />
+  <Navbar />
+  <router-view v-slot="{ Component }">
+    <transition name="route" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: { Navbar },
+};
+</script>
 <style>
 body {
   background: #f2f2f2;
@@ -13,5 +24,19 @@ body {
   max-width: 600px;
   margin: 0 auto;
   color: #555;
+}
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform-style: translateX(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 </style>
